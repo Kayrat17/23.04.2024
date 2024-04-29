87d1a6c3361893ba1ff2816162b3813e0c93ba1f
Kayrat17 <kayratsaduahas@gmail.com>
2024-04-29 15:53:31 +0500

    first commit

---
+++ bezrange.py
@@ -0,0 +1,10 @@
+def find_num(list1):
+    ind = 0
+    for i in list1:
+        if i == 6:
+            return ind
+        ind += 1
+
+print(find_num([8, 34, 6, 7, 67, 3, 5, 8]))
+a=6
+print ("элемент саны", a)
---
+++ tap1.py
@@ -0,0 +1,7 @@
+def find_num(list1):
+    for i in range(len(list1)):
+        if list1[i] == 6:
+            return i
+print(find_num([8,43,94,6,3]))
+a=6
+print("element sani",a)
